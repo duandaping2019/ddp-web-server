fedcdfdefab3a0eb65b23b2eac88945f4d60b023
duandaping <15022486479@163.com>
2021-01-10 13:11:01 +0800

    角色管理字段校正

--- server-base/src/main/java/ddp/entity/security/SysRoleEntity.java
+++ server-base/src/main/java/ddp/entity/security/SysRoleEntity.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.math.BigDecimal;
+import java.util.Date;
 
 
 /**
@@ -27,6 +28,27 @@ public class SysRoleEntity extends BaseEntity {
     @Column(name = "role_id", length = 32)
     private BigDecimal roleId;
 
+    @Column(name = "create_user_id", length = 32)
+    private String createUserId; //创建人Id
+
+    @Column(name = "create_user_name", length = 128)
+    private String createUserName; //创建人Name
+
+    @Column(name = "create_time", length = 19)
+    private Date createTime; //创建时间
+
+    @Column(name = "update_user_id", length = 32)
+    private String updateUserId; //更新人ID
+
+    @Column(name = "update_user_name", length = 128)
+    private String updateUserName; //更新人Name
+
+    @Column(name = "update_time", length = 32)
+    private Date updateTime; //更新时间
+
+    @Column(name = "del_flag", length = 1)
+    private Short delFlag; // 删除标记
+
     /**
     *角色标识
     */
@@ -39,50 +61,98 @@ public class SysRoleEntity extends BaseEntity {
     @Column(name = "role_name", length = 64)
     private String roleName;
 
-
     /**
-    * 设置角色ID
-    */
+     *角色名称
+     */
+    @Column(name = "role_desc", length = 256)
+    private String roleDesc;
+
     public BigDecimal getRoleId() {
-        return this.roleId;
+        return roleId;
     }
 
-    /**
-    * 获取角色ID
-    */
     public void setRoleId(BigDecimal roleId) {
         this.roleId = roleId;
     }
 
-    /**
-    * 设置角色标识
-    */
-    public String getRoleCode() {
-        return this.roleCode;
+    public String getCreateUserId() {
+        return createUserId;
     }
 
-    /**
-    * 获取角色标识
-    */
+    public void setCreateUserId(String createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public String getCreateUserName() {
+        return createUserName;
+    }
+
+    public void setCreateUserName(String createUserName) {
+        this.createUserName = createUserName;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateUserId() {
+        return updateUserId;
+    }
+
+    public void setUpdateUserId(String updateUserId) {
+        this.updateUserId = updateUserId;
+    }
+
+    public String getUpdateUserName() {
+        return updateUserName;
+    }
+
+    public void setUpdateUserName(String updateUserName) {
+        this.updateUserName = updateUserName;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Short getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(Short delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    public String getRoleCode() {
+        return roleCode;
+    }
+
     public void setRoleCode(String roleCode) {
         this.roleCode = roleCode;
     }
 
-    /**
-    * 设置角色名称
-    */
     public String getRoleName() {
-        return this.roleName;
+        return roleName;
     }
 
-    /**
-    * 获取角色名称
-    */
     public void setRoleName(String roleName) {
         this.roleName = roleName;
     }
 
+    public String getRoleDesc() {
+        return roleDesc;
+    }
 
-
+    public void setRoleDesc(String roleDesc) {
+        this.roleDesc = roleDesc;
+    }
 
 }
